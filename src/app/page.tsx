f11c04b8fe150c17b9920b63b1fa71a9c7c3230d
TIMILEYIN AWE <atmnation2004@gmail.com>
2024-02-13 14:38:02 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className="w-[100wh] h-[100vh] background flex justify-center items-center">
-        <div className="w-[90%] md:w-[75%] h-[80%] md:h-[70%] backdrop-blur-sm bg-[#673b5e]/60 rounded-md flex justify-center items-center p-10 md:px-24 flex-col leading-wide tracking-wider">
+        <div className="backdrop-blur-sm bg-[#673b5e]/60 rounded-md flex justify-center items-center p-10 md:px-24 flex-col leading-wide tracking-wider">
           <div className="">
             <h1 className="text-md font-medium text-black text-[1.2rem]">
               My Dearest, <br /> <br /> I hope this message finds you well, surrounded by the
